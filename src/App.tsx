8ec924872398604b674661be7dad3d5764539d72
Arnaldo Assis Ferreira <arnaldo_assisferreira@outlook.com>
2023-11-28 21:12:52 -0300

    layout setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import {ThemeProvider} from 'styled-components'
+import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { HashRouter } from 'react-router-dom'
+import { Router } from './Router'
 
 function App() {
-  
-
   return (
     <ThemeProvider theme={defaultTheme}>
-      Hello World
-
+      <HashRouter>
+        <Router />
+      </HashRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
