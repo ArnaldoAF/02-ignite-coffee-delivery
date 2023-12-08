f8f5c0a8bb4be994f8ac03ed9cd1b0c7cc39f547
Arnaldo Assis Ferreira <arnaldo_assisferreira@outlook.com>
2023-12-08 15:26:00 -0300

    link router

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,7 @@
 import { HeaderContainer } from './styles'
 import LogoImg from '../../assets/Logo.svg'
+import { NavLink } from 'react-router-dom'
+
 import { CartIcon } from '../CartIcon'
 import { MapPin } from 'phosphor-react'
 import { defaultTheme } from '../../styles/themes/default'
@@ -8,8 +10,11 @@ export function Header() {
   return (
     <HeaderContainer>
       <div>
-        <img src={LogoImg} alt="" />
+        <NavLink to="/" title="Timer">
+          <img src={LogoImg} alt="" />
+        </NavLink>
       </div>
+
       <div className="actionsDiv">
         <button className="locationButton">
           <MapPin size={22} weight="fill" color={defaultTheme['purple-dark']} />
