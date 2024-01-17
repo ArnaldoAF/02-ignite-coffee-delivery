59dfad16226b886231132725dae5cbdda20db593
Arnaldo Assis Ferreira <arnaldo_assisferreira@outlook.com>
2024-01-16 23:32:31 -0300

    form layout

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,9 +1,54 @@
-import { CheckoutContainer } from './styles'
+import { CheckoutContainer, BaseInput } from './styles'
+import { Title } from '../../styles/Typhography/Title'
+import { Text } from '../../styles/Typhography/Text'
+import { MapPinLine, CurrencyDollar } from 'phosphor-react'
+import { defaultTheme } from '../../styles/themes/default'
 
 export function Checkout() {
   return (
     <CheckoutContainer>
-      <h1>Checkout</h1>
+      <main>
+        <Title size="xs">Complete seu pedido</Title>
+        <article>
+          <section>
+            <header>
+              <MapPinLine size={22} color={defaultTheme['yellow-dark']} />
+              <div>
+                <Text size="m">Endereço de Entrega</Text>
+                <Text size="s">
+                  Informe o endereço onde deseja receber seu pedido
+                </Text>
+              </div>
+            </header>
+
+            <div>
+              <BaseInput className="w-40" placeholder="CEP" />
+              <BaseInput className="w-100" placeholder="Rua" />
+              <BaseInput className="w-40" placeholder="Número" />
+              <BaseInput className="w-60" placeholder="Complemento" />
+              <BaseInput className="w-40" placeholder="Bairro" />
+              <BaseInput className="w-40" placeholder="Cidade" />
+              <BaseInput className="w-20" placeholder="UF" />
+            </div>
+          </section>
+          <section>
+            <header>
+              <CurrencyDollar size={22} color={defaultTheme.purple} />
+              <div>
+                <Text size="m">Pagamento</Text>
+                <Text size="s">
+                  O pagamento é feito na entrega. Escolha a forma que deseja
+                  pagar
+                </Text>
+              </div>
+            </header>
+          </section>
+        </article>
+      </main>
+      <aside>
+        <Title size="xs">Cafés selecionados</Title>
+        <div></div>
+      </aside>
     </CheckoutContainer>
   )
 }
--- src/pages/Checkout/styles.ts
+++ src/pages/Checkout/styles.ts
@@ -1,3 +1,81 @@
 import styled from 'styled-components'
 
-export const CheckoutContainer = styled.div``
+export const CheckoutContainer = styled.div`
+  margin-top: 2.5rem;
+
+  display: flex;
+  gap: 2rem;
+
+  main {
+    width: 60%;
+
+    article {
+      display: flex;
+      flex-direction: column;
+      gap: 0.75rem;
+      margin-top: 1rem;
+
+      section {
+        padding: 2.5rem;
+        background: ${(props) => props.theme['base-card']};
+        border-radius: 6px;
+
+        header {
+          display: flex;
+          gap: 0.5rem;
+          margin-bottom: 2rem;
+        }
+
+        > div {
+          display: flex;
+          flex-wrap: wrap;
+          gap: 1rem;
+
+          .w-100 {
+            width: calc(100% - 1rem);
+          }
+
+          .w-60 {
+            width: calc(60% - 1rem);
+          }
+
+          .w-40 {
+            width: calc(40% - 1rem);
+          }
+
+          .w-20 {
+            width: calc(20% - 1rem);
+          }
+
+          .flex-1 {
+            flex: 1 1 auto;
+          }
+
+          .flex-3 {
+            flex: 3 1 auto;
+          }
+        }
+      }
+    }
+  }
+`
+
+export const BaseInput = styled.input`
+  background: ${(props) => props.theme['base-input']};
+  border-radius: 4px;
+  padding: 0.75rem;
+  border: 1px solid ${(props) => props.theme['base-button']};
+  font-size: 1rem;
+  color: ${(props) => props.theme['base-text']};
+  font-family: 'Roboto', sans-serif;
+  line-height: 130%;
+
+  &:focus {
+    box-shadow: none;
+    border-color: ${(props) => props.theme['yellow-dark']};
+  }
+
+  &::placeholder {
+    color: ${(props) => props.theme['base-label']};
+  }
+`
